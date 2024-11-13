ed52fa30b5cc9684144482a8d52f28d55971b310
kimjaewon <wjswls456456@naver.com>
2024-11-13 14:40:58 +0900

    feat : 테스트 코드 작성

---
+++ kickstart/ethereum/deploy.js
@@ -0,0 +1,20 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { Web3 } = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json')
+
+const provider = new HDWalletProvider('guard ceiling focus blood grab calm step fossil luggage coach apple museum', 'https://sepolia.infura.io/v3/9f8471c026464741ab3160f95537e8dd');
+
+const web3 = new Web3(provider)
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0])
+
+    const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface)).deploy({ data: compiledFactory.bytecode }).send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+    // provider.engine.stop()
+}
+
+deploy()
